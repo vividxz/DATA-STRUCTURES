7767ed7bba202af6631a1832e651ca4e6dfda337
vividxz <80575342+vividxz@users.noreply.github.com>
2021-11-15 06:41:12 +0530

    Update menuDriven_LL.cpp
    New Menu Driven Without Bug

--- Linked List/menuDriven_LL.cpp
+++ Linked List/menuDriven_LL.cpp
@@ -1,101 +1,203 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main();
-
-char ch;
-
-struct Node{
+struct node
+{
   int data;
-  struct Node *next;
+  struct node* next;
 };
 
-struct Node *Head = NULL;
+struct node* START = NULL;
 
-struct Node * createNode(void)
+struct node* CreateNode()
 {
-  struct Node * temp;
-  temp = (struct Node *)malloc(sizeof(struct Node));
+  struct node* temp;
+
+  temp = (struct node*)malloc(sizeof(struct node));
   return temp;
 }
-
-void insertNode(void)
+void InsertEnd()
 {
-  struct Node * temp , *traverse;
-  temp = createNode();
-  cout<<"Enter a number"<<endl;
+  struct node* temp = CreateNode();
+  cout<<"Enter a number\n";
   cin>>temp->data;
-  if(Head == NULL){
-    Head = temp;
+  temp -> next = NULL;
+  if(START == NULL)
+    START = temp;
+  else
+  {
+    struct node* t = START;
+    while(t->next != NULL)
+      t = t->next;
+    t -> next = temp;
+  }
+}
+void InsertStart()
+{
+  struct node* temp = CreateNode();
+  cout<<"Enter a number\n";
+  cin>>temp->data;
+  if(START == NULL)
+  {
+    START = temp;
     temp -> next = NULL;
   }
   else
   {
-    traverse = Head;
-    while(traverse->next != NULL)
-      traverse = traverse -> next ;
-    traverse -> next = temp;
-    temp -> next = NULL;
+    temp -> next = START;
+    START = temp;
   }
 }
-
-void deleteNode(void)
+void InsertBet()
 {
-  if(Head == NULL){
-    cout<<"List is empty\n";
-    int k = main();
-  }
-  int position;
-  cout<<"Enter position of element to delete\n";
-  cin>>position;
-  struct Node *traverse , *temp;
-  if(position == 1){
-    temp = Head;
-    Head = Head -> next;
-    free(temp);
-  }
-  else{
-    traverse = Head;
-    while(position-- -2)
-      traverse = traverse -> next;
-    temp = traverse -> next;
-    traverse -> next = temp -> next;
-    free(temp);
-  }
+  int pos;
+  cout<<"Enter position\n";
+  cin>>pos;
+  struct node* temp = CreateNode();
+  cout<<"Enter a number\n";
+  cin>>temp->data;
+  struct node* t = START;
+  pos -= 2;
+  while(pos--)
+    t = t -> next;
+  temp -> next =  t -> next;
+  t -> next = temp;
 }
-
-void displayNode(void)
+void InsertNode()
 {
-  struct Node * traverse = Head;
-  while(traverse != NULL)
+    cout<<"Enter 1 to insert in begining\n";
+    cout<<"Enter 2 to insert at end\n";
+    cout<<"Enter 3 to insert at a given position\n";
+    int choice;
+    cin>>choice;
+    switch (choice)
+    {
+      case 1:
+        InsertStart();
+        break;
+      case 2:
+        InsertEnd();
+        break;
+      case 3:
+        InsertBet();
+        break;
+      default:
+        cout<<"Enter appropriate choice\n";
+    }
+}
+void deleteLast()
+{
+  struct node* t = START;
+  while((t->next)->next != NULL)
+    t = t -> next;
+  free(t -> next);
+  t -> next = NULL;
+}
+void deleteFirst()
+{
+   struct node* temp = START;
+   START = START -> next;
+   free(temp);
+}
+void deleteBet()
+{
+  int key;
+  cout<<"Enter element to delete\n";
+  cin>>key;
+  struct node* t = START, *temp;
+  while(t != NULL)
   {
-    cout<<traverse -> data<<endl;
-    traverse = traverse -> next ;
+    if(t->next!= NULL && (t->next)->data == key)
+      break;
+    t = t->next;
+  }
+  if(t == NULL)
+    cout<<"Element Not found\n";
+  else
+  {
+    temp = t->next;
+    t->next = temp->next;
+    free(temp);
   }
 }
 
+void displayNodes()
+{
+  struct node* t = START;
+  while(t != NULL)
+  {
+    cout<<t->data<<endl;
+    t = t->next;
+  }
+}
+
+void deleteNode()
+{
+  cout<<"Enter 1 to delete first node\n";
+  cout<<"Enter 2 to delete last node\n";
+  cout<<"Enter 3 to delete a key node\n";
+  int choice;
+  cin>>choice;
+  switch (choice)
+  {
+    case 1:
+      deleteFirst();
+      break;
+    case 2:
+      deleteLast();
+      break;
+    case 3:
+      deleteBet();
+      break;
+    default:
+      cout<<"Enter appropriate choice\n";
+  }
+}
+void reverseList()
+{
+  struct node *prev = NULL, *current = START, *next;
+  while(current -> next != NULL)
+  {
+    next = current -> next;
+    current -> next = prev;
+    prev = current;
+    current = next;
+    next = next -> next;
+  }
+  current -> next = prev;
+  START = current;
+}
 int main()
 {
   int choice;
-  do{
-    cout<<"Enter 1 to Add element\nEnter 2 to Delete element\nEnter 3 to display elements\n";
+  char yn;
+  do
+  {
+    system("cls");
+    cout<<"Enter 1 to insert node\n";
+    cout<<"Enter 2 to delete node\n";
+    cout<<"Enter 3 to reverse list\n";
+    cout<<"Enter 4 to display list\n";
     cin>>choice;
-
-    switch (choice){
+    switch (choice)
+    {
       case 1:
-        insertNode();
+        InsertNode();
         break;
       case 2:
         deleteNode();
         break;
       case 3:
-        displayNode();
+        reverseList();
+        break;
+        case 4:
+        displayNodes();
         break;
       default:
         cout<<"Enter appropriate choice\n";
     }
-    cout<<"Continue ? y/n";
-    cin>>ch;
-  }while(ch == 'y');
-  return 0;
+    cout<<"Continue ? Y/N\n";
+    cin>>yn;
+  }while(yn == 'Y' || yn == 'y');
+
 }
